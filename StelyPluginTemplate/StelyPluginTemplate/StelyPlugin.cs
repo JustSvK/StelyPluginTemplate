856cba4a782f9da11d40e779036018f0d9fb5a0a
JustSvK <49286396+JustSvK@users.noreply.github.com>
2021-04-02 20:50:03 +0200

    Stely plugin template

---
+++ StelyPluginTemplate/StelyPluginTemplate/StelyPlugin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StelyPluginTemplate
+{
+    //Hello, this is the plugins tab!
+    //Here you can basically create your own plugin, which will work for Stely!
+    //So, you can code here basically everything, just do not change
+    //The class name(StelyPlugin) or the void name(Main)
+    //Btw, everything which is in Main will get only executed, so when you create new method, make sure to call it in there!
+    //-JustSvK
+    public class StelyPlugin
+    {
+        public static void Main()
+        {
+            MessageBox.Show("Hello from plugins!");
+        }
+    }
+}
